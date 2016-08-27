4ca40114039344352517da88f5447de3da126649
Oscar Esteban <code@oscaresteban.es>
2016-08-26 21:23:06 -0700

    Revert "pull onto temp docker branch for docker testing"

--- fmriprep/run_workflow.py
+++ fmriprep/run_workflow.py
@@ -76,11 +76,6 @@ def main():
         'skull_strip_ants': opts.skull_strip_ants,
         'output_dir': op.abspath(opts.output_dir),
     }
-    settings['work_dir'] = settings['output_dir'] # other wfs assume this is set
-
-
-    # set up logger
-    logger = logging.getLogger('cli')
 
     # set up logger
     logger = logging.getLogger('cli')
@@ -101,10 +96,19 @@ def main():
         if not op.exists(derivatives):
             os.makedirs(derivatives)
 
+        if not op.exists(settings['work_dir']):
+            os.makedirs(settings['work_dir'])
+
         if not op.exists(log_dir):
             os.makedirs(log_dir)
 
     logger.addHandler(logging.FileHandler(op.join(log_dir,'run_workflow')))
+
+    # Warn for default work/output directories
+    if (opts.work_dir == parser.get_default('work_dir') or
+          opts.output_dir == parser.get_default('output_dir')):
+        logger.warning("work-dir and/or output-dir not specified. Using " +
+                        opts.work_dir + " and " + opts.output_dir)
 
     # Set nipype config
     ncfg.update_config({
@@ -134,7 +138,7 @@ def main():
         subject_list = [op.basename(subdir)[4:] for subdir in glob.glob(
             op.join(settings['bids_root'], 'sub-*'))]
 
-    logger.info("subject list: " + str(subject_list))
+    logger.info("subject list: {}", ', '.join(subject_list))
 
     # Build main workflow and run
     preproc_wf = fmriprep_single(subject_list, settings=settings)
