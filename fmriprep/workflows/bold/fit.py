c3dde8aa5ff00ec72d2ee4099603cf8c46484466
Christopher J. Markiewicz <markiewicz@stanford.edu>
2023-10-13 16:44:53 -0400

    FIX: Sort on echo time

--- fmriprep/workflows/bold/fit.py
+++ fmriprep/workflows/bold/fit.py
@@ -521,8 +521,11 @@ def init_bold_native_wf(
     # Shortest echo first
     all_metadata, bold_files, echo_times = zip(
         *sorted(
-            (md := layout.get_metadata(bold_file), bold_file, md.get("EchoTime"))
-            for bold_file in listify(bold_series)
+            (
+                (md := layout.get_metadata(bold_file), bold_file, md.get("EchoTime"))
+                for bold_file in listify(bold_series)
+            ),
+            key=lambda _, _, echo_time: echo_time,
         )
     )
     multiecho = len(bold_files) > 1
