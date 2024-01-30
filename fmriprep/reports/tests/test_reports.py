3249c18010e5567c323aa93d7b90cd979f43fa32
celprov <celine.provins@unil.ch>
2024-01-30 13:55:00 +0100

    enh: add test to check that the error message has been incorporated in the report. enh: add test to check that the boilerplate has been incorporated in the reports. fix: condense all the tests in one implementation

--- fmriprep/reports/tests/test_reports.py
+++ fmriprep/reports/tests/test_reports.py
@@ -13,8 +13,9 @@ def data_dir():
     return Path(__file__).parents[2] / "data" / "tests"
 
 
+# Test with and without sessions' aggregation
 @pytest.mark.parametrize(
-    "aggr_ses_reports, expected_files, crash_txt",
+    "aggr_ses_reports, expected_files",
     [
         (
             3,
@@ -25,38 +26,55 @@ def data_dir():
                 "sub-001_ses-004_func.html",
                 "sub-001_ses-005_func.html",
             ],
-            False,
         ),
-        (
-            3,
-            [
-                "sub-001_anat.html",
-                "sub-001_ses-001_func.html",
-                "sub-001_ses-003_func.html",
-                "sub-001_ses-004_func.html",
-                "sub-001_ses-005_func.html",
-            ],
-            True,
-        ),
-        (4, ["sub-001.html"], False),
-        (4, ["sub-001.html"], True),
+        (4, ["sub-001.html"]),
     ],
 )
-def test_ReportSeparation(monkeypatch, data_dir, aggr_ses_reports, expected_files, crash_txt):
+# Test with and without crash file
+@pytest.mark.parametrize("error", (True, False))
+
+# Test with and without boilerplate
+@pytest.mark.parametrize("boilerplate", (True, False))
+
+# Test ses- prefix stripping
+@pytest.mark.parametrize(
+    "session_list", (["001", "003", "004", "005"], ["ses-001", "ses-003", "ses-004", "ses-005"])
+)
+# Test sub- prefix stripping
+@pytest.mark.parametrize("subject_label", ("001", "sub-001"))
+def test_ReportSeparation(
+    monkeypatch,
+    data_dir,
+    aggr_ses_reports,
+    expected_files,
+    error,
+    boilerplate,
+    session_list,
+    subject_label,
+):
     fake_uuid = "fake_uuid"
 
     # Test report generation with and without crash file
-    if crash_txt:
+    if error:
         # Copy the test crash file under the subject folder
-        dst_path = data_dir / f"work/reportlets/fmriprep/sub-001/{fake_uuid}/"
+        dst_path_e = data_dir / f"work/reportlets/fmriprep/sub-001/log/{fake_uuid}/"
         crash_file = "crash-20170905-182839-root-dvars-b78e9ea8-e295-48a1-af71-2d36afd9cebf.txt"
-        os.makedirs(dst_path, exist_ok=True)
-        shutil.copy2(data_dir / f"crash_files/{crash_file}", dst_path / crash_file)
+        os.makedirs(dst_path_e, exist_ok=True)
+        shutil.copy2(data_dir / f"crash_files/{crash_file}", dst_path_e / crash_file)
 
+    # Test report generation with and without boilerplate
+    if boilerplate:
+        # Copy the CITATION.html under the logs folder
+        dst_path_c = data_dir / f"work/reportlets/fmriprep/logs"
+        citation_file = "CITATION.html"
+        os.makedirs(dst_path_c, exist_ok=True)
+        shutil.copy2(data_dir / f"logs/{citation_file}", dst_path_c / citation_file)
+
+    # Patching
     monkeypatch.setattr(config.execution, 'aggr_ses_reports', aggr_ses_reports)
 
     def mock_session_list(*args, **kwargs):
-        return ['001', '003', '004', '005']
+        return session_list
 
     config.execution.layout = BIDSLayout(data_dir / "ds000005")
     monkeypatch.setattr(config.execution.layout, "get_sessions", mock_session_list)
@@ -65,66 +83,13 @@ def test_ReportSeparation(monkeypatch, data_dir, aggr_ses_reports, expected_file
     )
     output_dir = data_dir / "work/reportlets/fmriprep"
 
-    failed_reports = generate_reports(["sub-001"], output_dir, fake_uuid)
+    # Generate report
+    failed_reports = generate_reports([subject_label], output_dir, fake_uuid)
 
     # Verify that report generation was successfull
     assert not failed_reports
 
-    for expected_file in expected_files:
-        file_path = output_dir / expected_file
-        assert file_path.is_file(), f"Expected file {expected_file} is missing"
-
-    # Check if there are no unexpected HTML files
-    unexpected_files = {
-        file.name for file in output_dir.iterdir() if file.suffix == '.html'
-    } - set(expected_files)
-
-    assert not unexpected_files, f"Unexpected HTML files found: {unexpected_files}"
-
-    # Delete copied crash file
-    if crash_txt:
-        os.remove(dst_path / crash_file)
-        os.rmdir(dst_path)
-
-    # Delete generated HTML files
-    for file in output_dir.iterdir():
-        if file.suffix == '.html':
-            os.remove(file)
-
-
-@pytest.mark.parametrize(
-    "subject_label, session_list",
-    [
-        ("sub-001", ["001", "003", "004", "005"]),
-        ("sub-001", ["ses-001", "ses-003", "ses-004", "ses-005"]),
-        ("001", ["001", "003", "004", "005"]),
-        ("001", ["ses-001", "ses-003", "ses-004", "ses-005"]),
-    ],
-)
-def test_PrefixStripping(monkeypatch, data_dir, subject_label, session_list):
-    monkeypatch.setattr(config.execution, 'aggr_ses_reports', 3)
-
-    def mock_session_list(subject):
-        return session_list
-
-    config.execution.layout = BIDSLayout(data_dir / "ds000005")
-    monkeypatch.setattr(config.execution.layout, "get_sessions", mock_session_list)
-
-    output_dir = data_dir / "work/reportlets/fmriprep"
-
-    failed_reports = generate_reports([subject_label], output_dir, "fake_uuid")
-
-    # Verify that report generation was successfull
-    assert not failed_reports
-
-    # Drop ses- prefixes
-    session_list = [ses[4:] if ses.startswith("ses-") else ses for ses in session_list]
-    # Drop sub- prefix
-    subject_label = subject_label.lstrip("sub-")
-
-    expected_files = [f"sub-{subject_label}_anat.html"] + [
-        f"sub-{subject_label}_ses-{session}_func.html" for session in session_list
-    ]
+    # Check that all expected files were generated
     for expected_file in expected_files:
         file_path = output_dir / expected_file
         assert file_path.is_file(), f"Expected file {expected_file} is missing"
@@ -134,6 +99,30 @@ def test_PrefixStripping(monkeypatch, data_dir, subject_label, session_list):
         file.name for file in output_dir.iterdir() if file.suffix == '.html'
     } - set(expected_files)
     assert not unexpected_files, f"Unexpected HTML files found: {unexpected_files}"
+
+    if boilerplate:
+        # Verify that the keywords indicating the boilerplate is reported are present in the HTML
+        with open(output_dir / expected_files[0], 'r', encoding='utf-8') as file:
+            html_content = file.read()
+            assert (
+                "The boilerplate text was automatically generated" in html_content
+            ), f"The file {file} did not contain the reported error."
+
+        # Delete copied citation file
+        os.remove(dst_path_c / citation_file)
+        os.rmdir(dst_path_c)
+
+    if error:
+        # Verify that the keywords indicating a reported error are present in the HTML
+        with open(output_dir / expected_files[0], 'r', encoding='utf-8') as file:
+            html_content = file.read()
+            assert (
+                "One or more execution steps failed" in html_content
+            ), f"The file {file} did not contain the reported error."
+
+        # Delete copied crash file
+        os.remove(dst_path_e / crash_file)
+        os.rmdir(dst_path_e)
 
     # Delete generated HTML files
     for file in output_dir.iterdir():
