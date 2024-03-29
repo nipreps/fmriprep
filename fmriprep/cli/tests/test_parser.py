1b560c4be14330de46b1b36a7367da9d3627ca60
Taylor Salo <tsalo006@fiu.edu>
2024-03-29 09:46:04 -0400

    Fix mistake.

--- fmriprep/cli/tests/test_parser.py
+++ fmriprep/cli/tests/test_parser.py
@@ -254,7 +254,7 @@ def test_derivatives(tmp_path):
     # Providing --derivatives with names should use them
     temp_args = args + [
         '--derivatives',
-        f'smriprep={str(bids_path / 'derivatives/smriprep')}',
+        f'smriprep={str(bids_path / "derivatives/smriprep")}',
     ]
     opts = parser.parse_args(temp_args)
     assert opts.derivatives == {'smriprep': str(bids_path / 'derivatives/smriprep')}
