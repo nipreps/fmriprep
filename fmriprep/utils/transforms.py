bcc7d3e2c3bcf8635febafdf97ce5727d67b2cec
mathiasg <mathiasg@stanford.edu>
2024-06-05 15:08:05 -0400

    FIX: Catch cases of unexpected resampling

--- fmriprep/utils/transforms.py
+++ fmriprep/utils/transforms.py
@@ -71,6 +71,16 @@ def load_ants_h5(filename: Path) -> nt.base.TransformBase:
 
     fixed_params = transform2['TransformFixedParameters'][:]
 
+    spacing = fixed_params[6:9]
+    direction = fixed_params[9:]
+
+    # Supported spacing
+    if not np.array_equal(spacing, np.array([1.0, 1.0, 1.0])):
+        raise ValueError(f'Unexpected spacing: {spacing}')
+
+    if not np.array_equal(direction, np.diag([-1, -1, -1])):
+        raise ValueError(f'Asymmetric direction matrix: {direction}')
+
     shape = tuple(fixed_params[:3].astype(int))
     # ITK stores warps in Fortran-order, where the vector components change fastest
     # Nitransforms expects 3 volumes, not a volume of three-vectors, so transpose
