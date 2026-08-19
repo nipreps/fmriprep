ce3f9a1e2c458e80e7a1ec71c0d1d35ca268b202
mathiasg <mathiasg@stanford.edu>
2026-08-19 14:51:18 -0400

    tst: additional derivative handling

--- fmriprep/utils/tests/test_derivative_cache.py
+++ fmriprep/utils/tests/test_derivative_cache.py
@@ -5,13 +5,22 @@ import pytest
 from fmriprep.utils import bids
 
 
-@pytest.mark.parametrize('desc', ['hmc', 'coreg'])
-def test_baseline_found_as_str(tmp_path: Path, desc: str):
+@pytest.mark.parametrize(
+    ('key', 'ents'),
+    [
+        ('hmc', 'space-run_desc-hmc'),
+        ('run', 'space-run'),
+        # legacy space-less names
+        ('hmc', 'desc-hmc'),
+        ('run', 'desc-coreg'),
+    ],
+)
+def test_baseline_found_as_str(tmp_path: Path, key: str, ents: str):
     subject = '0'
     task = 'rest'
 
     to_find = tmp_path.joinpath(
-        f'sub-{subject}', 'func', f'sub-{subject}_task-{task}_desc-{desc}_boldref.nii.gz'
+        f'sub-{subject}', 'func', f'sub-{subject}_task-{task}_{ents}_boldref.nii.gz'
     )
     to_find.parent.mkdir(parents=True)
     to_find.touch()
@@ -24,22 +33,23 @@ def test_baseline_found_as_str(tmp_path: Path, desc: str):
     }
 
     derivs = bids.collect_derivatives(derivatives_dir=tmp_path, entities=entities)
-    expected = {f'{desc}_boldref': str(to_find), 'transforms': {}}
-    if desc == 'coreg':
-        # coreg_boldref is aliased to the run_boldref key for legacy inputs
-        expected['run_boldref'] = str(to_find)
-    assert dict(derivs) == expected
+    assert dict(derivs) == {f'{key}_boldref': str(to_find), 'transforms': {}}
 
 
-@pytest.mark.parametrize('xfm', ['run2fmap', 'boldref2anat', 'hmc'])
-def test_transforms_found_as_str(tmp_path: Path, xfm: str):
+@pytest.mark.parametrize(
+    ('xfm', 'fromto'),
+    [
+        ('hmc', 'from-orig_to-boldref'),
+        ('run2fmap', 'from-run_to-auto00000'),
+        ('boldref2anat', 'from-run_to-anat'),
+        # legacy from-boldref names
+        ('run2fmap', 'from-boldref_to-auto00000'),
+        ('boldref2anat', 'from-boldref_to-anat'),
+    ],
+)
+def test_transforms_found_as_str(tmp_path: Path, xfm: str, fromto: str):
     subject = '0'
     task = 'rest'
-    fromto = {
-        'hmc': 'from-orig_to-boldref',
-        'run2fmap': 'from-run_to-auto00000',
-        'boldref2anat': 'from-boldref_to-anat',
-    }[xfm]
 
     to_find = tmp_path.joinpath(
         f'sub-{subject}', 'func', f'sub-{subject}_task-{task}_{fromto}_mode-image_xfm.txt'
@@ -60,3 +70,28 @@ def test_transforms_found_as_str(tmp_path: Path, xfm: str):
         fieldmap_id='auto_00000',
     )
     assert derivs == {'transforms': {xfm: str(to_find)}}
+
+
+@pytest.mark.parametrize(('coreg_space', 'ses_ents'), [('session', '_ses-A'), ('subject', '')])
+def test_group_boldref2anat_found_for_run(tmp_path: Path, coreg_space: str, ses_ents: str):
+    subject = '0'
+    task = 'rest'
+
+    func = tmp_path.joinpath(f'sub-{subject}', *(['ses-A'] if ses_ents else []), 'func')
+    func.mkdir(parents=True)
+    to_find = func / (
+        f'sub-{subject}{ses_ents}_from-{coreg_space}_to-anat_mode-image_desc-coreg_xfm.txt'
+    )
+    to_find.touch()
+
+    entities = {
+        'subject': subject,
+        'session': 'A',
+        'task': task,
+        'run': '01',
+        'suffix': 'bold',
+        'extension': '.nii.gz',
+    }
+
+    derivs = bids.collect_derivatives(derivatives_dir=tmp_path, entities=entities)
+    assert derivs == {'transforms': {'boldref2anat': str(to_find)}}
